a474bce63e082b8b9426e42a2448946f54324b39
leandro47 <leandrosilva47@live.com>
2021-08-27 20:51:10 -0300

    first commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required']
+        ]);
+
+        if (Auth::attempt($request->only('email', 'password'))){
+            return response()->json(Auth::user(), 200);
+        }
+        throw ValidationException::withMessages([
+            'email' =>['The provided credentials are incorect.']
+        ]);
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+    }
+
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => ['required'],
+            'email' => ['required', 'email', 'unique:users'],
+            'password' =>['required', 'min:6', 'confirmed']
+        ]);
+
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+    }
+}
